af70f3528599c405cd28f2bbb4d6880288101bb9
Maxim42-00 <alfred.hammersmit@yandex.ru>
2020-04-21 13:41:57 +0300

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,24 @@
+﻿import React from "react";
+import "./Home.css";
+import take from "./take.jpg";
+
+class Home extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="Home">
+                <div>
+                    <div> <img src={take} /> </div>
+                    <div className="btn_panel">
+                        <div className="btn"> Вход </div>
+                        <div className="btn"> Регистрация </div>
+                    </div>
+                    
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Home;
